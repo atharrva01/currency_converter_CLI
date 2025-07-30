c47c9353e5b2bda89a72ebfddb99c12ef874ff6a
atharrva01 <atharvaborade568@gmail.com>
2025-07-30 21:10:33 +0530

    Currency converter CLI

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description="This is a currency converter CLI")
+
+parser.add_argument("--currency" , required= True , help = "Add the currency from which you want your amount to get converted.")
+parser.add_argument("--to" , required= True , help = "Add the currency to which you want your amount to get converted.")
+parser.add_argument("--amount" , required= True , help = "Add the amount which you want to get converted.")
+
+args = parser.parse_args()
+
+api = "9220769307023feca246520f"
+
+currency = (args.currency).upper()
+
+convert_currency = (args.to).upper()
+
+amount = float((args.amount))
+
+url = f"https://v6.exchangerate-api.com/v6/{api}/latest/{currency}"
+
+data = requests.get(url).json()
+
+try: 
+
+    convert_currency_val = data["conversion_rates"][convert_currency]
+
+    result = amount * convert_currency_val
+
+    print(f"The amount converted from {currency} to {convert_currency}, the converted amount is {round(result ,2)}")
+    
+except Exception as e:
+    print(f"{e}, Enter correct Currency code.")
+    exit()
